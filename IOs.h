b4b37f45a93fa624ec6787e4e2611edb9dd816e1
BlueEvil777 <zecoltonisawesome@gmail.com>
2020-11-04 21:51:21 -0700

    Updated flags

--- IOs.h
+++ IOs.h
@@ -14,10 +14,13 @@
 
 #define IOS_PB1_PRESSED 0x1
 #define IOS_PB1_RELEASED 0x2
-#define IOS_PB2_PRESSED 0x4
-#define IOS_PB2_RELEASED 0x8
-#define IOS_PB3_PRESSED 0x10
-#define IOS_PB3_RELEASED 0x20
+#define IOS_PB1_DEBOUNCE 0x4
+#define IOS_PB2_PRESSED 0x8
+#define IOS_PB2_RELEASED 0x10
+#define IOS_PB2_DEBOUNCE 0x20
+#define IOS_PB3_PRESSED 0x40
+#define IOS_PB3_RELEASED 0x80
+#define IOS_PB3_DEBOUNCE 0x100
 
 #define __set_bits(r,b) (r |= (b))
 #define __clear_bits(r,b) (r &= (~(b)))
@@ -28,7 +31,7 @@
 extern uint16_t IOS_FLAGS;
 
 void IOinit();
-void IOcheck();
+void IO_LED();
 
 #endif XC_HEADER_TEMPLATE_H
 
