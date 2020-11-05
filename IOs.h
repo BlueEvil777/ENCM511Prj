9faedf4f11d237d071f6e115e1f5943288f8a6c6
BlueEvil777 <zecoltonisawesome@gmail.com>
2020-11-05 15:31:05 -0700

    ...

--- IOs.h
+++ IOs.h
@@ -13,18 +13,23 @@
 #include <xc.h> // include processor files - each processor file is guarded.  
 
 #define IOS_PB1_PRESSED 0x1
-#define IOS_PB1_RELEASED 0x2
-#define IOS_PB1_DEBOUNCE 0x4
-#define IOS_PB2_PRESSED 0x8
-#define IOS_PB2_RELEASED 0x10
-#define IOS_PB2_DEBOUNCE 0x20
-#define IOS_PB3_PRESSED 0x40
-#define IOS_PB3_RELEASED 0x80
-#define IOS_PB3_DEBOUNCE 0x100
+#define IOS_PB1_DEBOUNCE 0x2
+#define IOS_PB2_PRESSED 0x4
+#define IOS_PB2_DEBOUNCE 0x8
+#define IOS_PB3_PRESSED 0x10
+#define IOS_PB3_DEBOUNCE 0x20
 
-#define __set_bits(r,b) (r |= (b))
-#define __clear_bits(r,b) (r &= (~(b)))
-#define __read_bits(r,b) (r & b)
+#define IOS_NEW_BUTTON_STATE 0x40
+
+#define IOS_PB_FLAG_OFFSET 0
+#define IOS_PB_FLAG_WIDTH 2
+#define IOS_PB_PRESSED 0x1
+#define IOS_PB_DEBOUNCE 0x2
+#define IOS_PB_FLAGS 0x3f
+
+#define __set_bits(r,o,b) (r |= ((b)<<(o)))
+#define __clear_bits(r,o,b) (r &= (~((b)<<(o))))
+#define __read_bits(r,o,b) (r & ((b)<<(o)))
 
 ///////// Global Variables /////////////////
 
